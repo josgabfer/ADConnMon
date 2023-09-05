1da009a3a1f793fa7d8b35a246716bedba55e4ae
Deathstars22 <josgabfer@outlook.com>
2023-09-05 10:43:00 -0600

    Added UmbrellaAPI package integration

--- ADConnectorMonitor.py
+++ ADConnectorMonitor.py
@@ -1,27 +1,47 @@
-import smtplib
+"""
+Copyright (c) 2023 Cisco and/or its affiliates.
+This software is licensed to you under the terms of the Cisco Sample
+Code License, Version 1.1 (the "License"). You may obtain a copy of the
+License at
+
+https://developer.cisco.com/docs/licenses
+
+All use of the material herein must be in accordance with the terms of
+the License. All rights not expressly granted by the License are
+reserved. Unless required by applicable law or agreed to separately in
+writing, software distributed under the License is distributed on an "AS
+IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+or implied.
+"""
+
+''' Get summary information from VAs to detect AD Connectors in error state '''
+
+
+# Export/Set the environment variables
+from umbrella.UmbrellaAPI import UmbrellaAPI
 from email.message import EmailMessage
-from dotenv import dotenv_values, find_dotenv
-from getToken import generate_auth_string
-import requests
-from requests.models import HTTPError
-from termcolor import colored
-import http.client as http_client
+import smtplib
+import datetime
+import json
+import os
+client_id = os.environ.get('API_KEY')
+client_secret = os.environ.get('API_SECRET')
+email_address = os.environ.get('EMAIL_ADDRESS')
+passw = os.environ.get('PASS')
+recipients = os.environ.get('RECIPIENTS')
+token_url = os.environ.get(
+    'TOKEN_URL') or 'https://api.umbrella.com/auth/v2/token'
+cur_dir = os.environ.get('CISCO_SAMPLE_DIR') or os.getcwd()
 
 
-config = dotenv_values(".env")
-
-EMAIL_ADDRESS = config['EMAIL_ADDRESS']
-PASS = config['PASS']
-RECIPIENTS = config['RECIPIENTS']
-count = 0
-
-def send_email(connectorName):
+def send_email(component):
     """This function will send an alert to the desired recipients"""
     msg = EmailMessage()
     msg['Subject'] = 'AD Connector Error Found!'
-    msg['From'] = EMAIL_ADDRESS
-    msg['To'] = RECIPIENTS
-    msg.set_content(f'Connection Error found in {connectorName}. Please check your Umbrella Dashboard and connectivity to all DCs')
+    msg['From'] = email_address
+    msg['To'] = recipients
+    msg.set_content(
+        f'Connection Error found in {component}. Please check your Umbrella Dashboard and connectivity to all DCs')
 
     msg.add_alternative("""
     <!DOCTYPE >
@@ -33,8 +53,10 @@ def send_email(connectorName):
         <title>AD Connector Monitor</title>
     </head>
     <body>
-        <h1>AD Connector Error</h1>
-        <p>The AD Connector Monitor script detected an error with: """ + connectorName + """. Please check your Umbrella dashboard.</p>
+        <h1>AD Connector Error Detected at """ + str(datetime.datetime.now()) + """</h1>
+        <p>The AD Connector Monitor script detected an error with: """ + component + """.</p>
+        <br/>
+        <p>Please check your Umbrella dashboard.</p>
 
         <style type="text/css">
             body{
@@ -47,54 +69,34 @@ def send_email(connectorName):
     </html>
     """, subtype='html')
 
-    
-    with smtplib.SMTP_SSL('smtp.gmail.com',465) as smtp:
-        smtp.login(EMAIL_ADDRESS,PASS)
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+        smtp.login(email_address, passw)
         print('login success')
         smtp.send_message(msg)
-        print("Email has been sent to: ", RECIPIENTS)
+        print("Email has been sent to: ", recipients)
 
-def get_request(): 
-    """This function will send a GET request to get a list of all AD integration components"""
-    url = "https://api.umbrella.com/deployments/v2/virtualappliances"
-    global count
-    count += 1
-    if (count == 3):
-        print(colored(f"\nMaximum attempts to reach {url} exceeded", "red"))
-        return
-    config = dotenv_values(find_dotenv())
-    token = config.get('TOKEN')
-    if (token == None):
-        token = (generate_auth_string())
-    headers = {
-            "Authorization": "Bearer " + token,
-            "Content-Type": "application/json"
-        }
-    print(colored(f"Contacting API: {url}", "green"))
-    print("\n")
+
+# main
+if __name__ == '__main__':
+
+    # Exit out if the required API_KEY and API_SECRET are not set in the environment
+    for var in ['API_SECRET', 'API_KEY']:
+        if os.environ.get(var) == None:
+            print("Required environment variable: {} not set".format(var))
+            exit()
 
     try:
-        response = requests.get(url, headers=headers)
-        if (response.status_code == 401 or response.status_code == 403):
-            token = generate_auth_string()
-            return get_request()
-        elif (response.status_code == 200):
-            print(colored("Get request successfully executed!", "green"))
-            print("\n")
-            return response.json()
-    except HTTPError as httperr:
-        print(colored(f'HTPP error occured: {httperr}','red'))
-        
+        # Step 1: Create the API client
+        umbrella_api = UmbrellaAPI(token_url, client_id, client_secret)
+
+        # Step 2: Send a request checking for status of the AD Connector
+        umbrella_AD_endpoint = 'deployments/v2/virtualappliances'
+        adComponents = umbrella_api.ReqGet(umbrella_AD_endpoint).json()
+
+        # Step 3: Check the status of the components, call send_email() if error is found in a component
+        for component in adComponents:
+            if (component.get("type") == "connector" and component.get("health") == "error"):
+                send_email(component.get("name"))
 
     except Exception as e:
-        print(colored(f'HTPP error occured: {e}','red'))
-
-def alert():
-    """This function will search for all the AD connectors in error state to call the send email function"""
-    adComponents = get_request()
-    for component in adComponents:
-        if (component.get("type") == "connector" and component.get("health") == "error"):
-            send_email(component.get("name"))
-
-if __name__ == '__main__':
-    alert()
+        raise (e)
